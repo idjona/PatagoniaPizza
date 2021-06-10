c709f0e53f8c7f82514a524145a994575750caec
Jonathan <jonathanisladiaz@gmail.com>
2021-06-09 22:57:30 -0400

    layouts ok

--- app/src/main/java/com/jona/patagoniapizza/ActivityRegistro.java
+++ app/src/main/java/com/jona/patagoniapizza/ActivityRegistro.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityRegistro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_seleccion_pizza);
-
-
+        setContentView(R.layout.activity_registro);
     }
 }
